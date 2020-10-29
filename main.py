c54cbc442b7b75c6ea75d62172856c133debd2bf
evgnngve-ii <66582478+evgnngve-ii@users.noreply.github.com>
2020-10-28 18:17:54 -0700

    Added first iteration of intersection check

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ y = 0.33333333333333
 # Counters
 intervalLimit = 10  # Amount of intervals in a simulation run
 totalNumberOfCarsPassed = 0  # Amount of cars that passed in the entire run
-currentIntervalNumber = 1 #What current turn we are on
+currentIntervalNumber = 1  # What current turn we are on
 
 
 ################################ FUNCTIONS #############################
@@ -48,12 +48,12 @@ class Car:
 
 
 ##################INITIALIZE PHASE:################################################
-# Initialize the intersection
+# Initialize the intersection; 0 is that part of the road isn't occupied; 1 means it is.
 # intersection = [A,B,C,D]
-intersection = [True, True, True, True]
+intersection = [0,1,0,0]
 
 # Initialize our cars
-# Y and are just empty placeholders
+# D and G are just empty placeholders
 carOne = Car(0, 'D', 'G', 0)
 carTwo = Car(0, 'D', 'G', 1)
 carThree = Car(0, 'D', 'G', 2)
@@ -78,70 +78,88 @@ for i in range(len(carLot)):
     # Multiply by -1 so that priority queue puts the "negative-most" values at top
     carOrdering.put(-carRank, str(i))  # More wait time => more negative => More priority
 
+
 # carLotState(carLot)
 
 ##############SIMULATION PHASE###################################################
-#Take note, index is also our position; AKA c1 on bottom, c2 on left, c3 on top, c4 on right.
+# Take note, index is also our position; AKA c1 on bottom, c2 on left, c3 on top, c4 on right.
 
-#Checks if the car going Left is at position c1,c2,c3,c4 and if it can go
-def canTurnLeft(car): #2a
-    if car.carIndex == 0: #If car is C1
+# Checks if the car going Left is at position c1,c2,c3,c4 and if it can go
+def canTurnLeft(car):  # 2a ...(Can car at current position go, given intersection state?)
+    if car.carIndex == 0:  # If car is C1 going left #0 == 0:
+        if intersection[0] == 0:
+            print("A is free")
+            if intersection[1] == 0:
+                print("B is free")
+                if intersection[3] == 0:
+                    print("D is free")
+                    return "True"
+    if car.carIndex == 1:  # If car is C2 going left
         pass
-    if car.carIndex == 1: #If car is C2
+    if car.carIndex == 2:  # If car is C3 going left
         pass
-    if car.carIndex == 2: #If car is C3
+    if car.carIndex == 3:  # If car is C4 going left
         pass
-    if car.carIndex == 3: #If car is C4
-        pass
+
 
 # Checks if the car going Right is at position c1,c2,c3,c4 and if it can go
-def canTurnRight():#2b
-    if car.carIndex == 0:
+def canTurnRight(car):  # 2b
+    if car.carIndex == 0:  # If car is C1 going right
         pass
-    if car.carIndex == 1:
+    if car.carIndex == 1:  # If car is C2 going right
         pass
-    if car.carIndex == 2:
+    if car.carIndex == 2:  # If car is C3 going right
         pass
-    if car.carIndex == 3:
+    if car.carIndex == 3:  # If car is C4 going right
         pass
 
-#Checks if the car going Straight is at position c1,c2,c3,c4 and if it can go
-def canTurnStraight():#2c
-    if car.carIndex == 0:
+
+# Checks if the car going Straight is at position c1,c2,c3,c4 and if it can go
+def canTurnStraight(car):  # 2c
+    if car.carIndex == 0:  # If car is C1 going straight
         pass
-    if car.carIndex == 1:
+    if car.carIndex == 1:  # If car is C2 going straight
         pass
-    if car.carIndex == 2:
+    if car.carIndex == 2:  # If car is C3 going straight
         pass
-    if car.carIndex == 3:
+    if car.carIndex == 3:  # If car is C4 going straight
         pass
 
 
 # Checks the direction of the car, and will use a helper function to determine if the car can pass, from it's position.
 # Returns false other wise
-def checkDirection(car): #(1)
+def checkDirection(car):  # (1) If we are going L (or S or R)...
     if car.direction == 'L':
-        #print("Car# " + str(car.carIndex) + " going LEFT")
+        # print("Car# " + str(car.carIndex) + " going LEFT")
         canTurnLeft(car)
 
     if car.direction == 'S':
-        #print("Car# " + str(car.carIndex) + " going STRAIGHT")
+        # print("Car# " + str(car.carIndex) + " going STRAIGHT")
+        canTurnStraight(car)
 
     if car.direction == 'R':
-        #print("Car# " + str(car.carIndex) + " going RIGHT")
+        # print("Car# " + str(car.carIndex) + " going RIGHT")
+        canTurnRight(car)
 
 
-print("car 1 is going " + str(carLot[0].direction) + ". It is: " + checkDirection(carLot[0]))
+#print("car 1 is going " + str(carLot[0].direction) + ". It is: " + checkDirection(carLot[0]))
+TestCarC1Left = Car(0, 'L', 'G', 0)
+
+booleanCheck = (checkDirection(TestCarC1Left))
+didPass = str(checkDirection(TestCarC1Left))
+
+print(booleanCheck)
+#print(didPass)
+
+print("On an empty intersection, car 1 going left : " + didPass)
 
 
-
-
-#Run 10 times A SIMULATION GOES HERE
+# Run 10 times A SIMULATION GOES HERE
 # def intervalRun(carOrdering): # ToDo
-  # pass
+# pass
 
-#for x in range(intervalLimit): #ToDo
- #   intervalRun(carOrdering)
+# for x in range(intervalLimit): #ToDo
+#   intervalRun(carOrdering)
 
 ###############DEBUGGING STUFF################
 # print("Directions assigned! The assigned directions are")
