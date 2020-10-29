a395eeeff389e032d847684319a5dc38adbaa52a
evgnngve-ii <66582478+evgnngve-ii@users.noreply.github.com>
2020-10-28 21:20:30 -0700

    hopefully finished; 1 error left....
    local variable 'currentIntervalNumber' referenced before assignment

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ y = 0.33333333333333
 intervalLimit = 10  # Amount of intervals in a simulation run
 totalNumberOfCarsPassed = 0  # Amount of cars that passed in the entire run
 currentIntervalNumber = 1  # What current turn we are on
-recordedPasses = []  # Filled with tuples; (car#, wait time, turn #)
+recordedPasses = ["dummy"]  # Filled with tuples; (car#, wait time, turn #)
 
 
 ################################ FUNCTIONS #############################
@@ -21,6 +21,7 @@ recordedPasses = []  # Filled with tuples; (car#, wait time, turn #)
 def carLotState():
     for car in carLot:
         print("Car " + str(car.carIndex) + " W:" + str(car.waitTime) + " d:" + car.direction + " g:" + car.carPassed)
+
 
 # Checks if the car going Left is at position c1,c2,c3,c4 and if it can go
 def canTurnLeft(car):  # 2a ...(Can car at current position go, given intersection state?)
@@ -38,6 +39,7 @@ def canTurnLeft(car):  # 2a ...(Can car at current position go, given intersecti
         if intersection[0] == intersection[1] == intersection[2] == 0:
             return True
 
+
 # Checks if the car going Right is at position c1,c2,c3,c4 and if it can go
 def canTurnRight(car):  # 2b
     if car.carIndex == 0:  # If car is C1 going right
@@ -53,6 +55,7 @@ def canTurnRight(car):  # 2b
         if intersection[1] == 0:
             return True
 
+
 # Checks if the car going Straight is at position c1,c2,c3,c4 and if it can go
 def canTurnStraight(car):  # 2c
     if car.carIndex == 0:  # If car is C1 going straight
@@ -67,6 +70,7 @@ def canTurnStraight(car):  # 2c
     if car.carIndex == 3:  # If car is C4 going straight
         if intersection[0] == intersection[1] == 0:
             return True
+
 
 # Checks the direction of the car, and will use a helper function to determine if the car can pass, from it's position.
 # Returns false other wise
@@ -84,7 +88,8 @@ def checkDirection(car):  # (1) If we are going L (or S or R)...
         # print("Car# " + str(car.carIndex) + " going RIGHT")
         return canTurnRight(car)
 
-#Update intersection occupied by a car going left (or right or straight) by setting them to 1's
+
+# Update intersection occupied by a car going left (or right or straight) by setting them to 1's
 def updateLeft(carPassed):
     if carPassed.carIndex == 0:  # If car is C1 going left #0 == 0:
         intersection[0] = 1
@@ -102,25 +107,27 @@ def updateLeft(carPassed):
         intersection[0] = 1
         intersection[1] = 1
         intersection[2] = 1
- 
+
+
 def updateRight(carPassed):
     if carPassed.carIndex == 0:  # If car is C1 going right
-         intersection[3] == 1
+        intersection[3] == 1
     if carPassed.carIndex == 1:  # If car is C2 going right
-         intersection[2] == 1
+        intersection[2] == 1
     if carPassed.carIndex == 2:  # If car is C3 going right
-         intersection[0] == 1
+        intersection[0] == 1
     if carPassed.carIndex == 3:  # If car is C4 going right
-         intersection[1] == 1
+        intersection[1] == 1
+
 
 def updateStraight(carPassed):
-    if carPassed.carIndex == 0:  #  car C1
+    if carPassed.carIndex == 0:  # car C1
         intersection[1] = 1
         intersection[3] = 1
-    if carPassed.carIndex == 1:  #car c2
+    if carPassed.carIndex == 1:  # car c2
         intersection[2] = 1
         intersection[3] = 1
-    if carPassed.carIndex == 2:  #  car C3
+    if carPassed.carIndex == 2:  # car C3
         intersection[0] = 1
         intersection[2] = 1
     if carPassed.carIndex == 3:  # car C4
@@ -128,8 +135,7 @@ def updateStraight(carPassed):
         intersection[1] = 1
 
 
-
-#Works similar to checkDirection, except we want to update the space taken up by an intersection in an interval
+# Works similar to checkDirection, except we want to update the space taken up by an intersection in an interval
 def updateIntersection(carPassed):
     if carPassed.direction == 'L':
         updateLeft(carPassed)
@@ -137,7 +143,6 @@ def updateIntersection(carPassed):
         updateStraight(carPassed)
     if carPassed.direction == 'R':
         updateRight(carPassed)
-
 
 # Given a random number, deduce the direction based off our current x,y parameters.
 def assignDirection(n):
@@ -156,6 +161,22 @@ def assignDirection(n):
         d = 'R'
 
     return d
+
+# Will return a priority que with ordered cars in the current state
+# Do ordering of cars     #Stretch Goal: Implement the more robust way of the ordering
+def calculateNextOrder():
+    carOrdering = PriorityQueue(4)
+    for i in range(len(carLot)):
+        index = i;
+        adjustIndex = -(carLot[i].carIndex)  # make index value so that we can prioritize lower indexes
+        carRank = (carLot[i].waitTime + adjustIndex)  # Calculate the rank of the given car; Wait time + lower index
+        # print("CAR#"+str(i)+" has rank of " + str(carRank))
+        # Multiply by -1 so that priority queue puts the "negative-most" values at top
+        carOrdering.put(-carRank, str(index))  # More wait time => more negative => More priority
+    return carOrdering
+
+
+
 
 # Define a "car" w/ waitTime (w), direction (d), canGo(g)
 class Car:
@@ -184,18 +205,8 @@ carLot = [carOne, carTwo, carThree, carFour]
 for i in range(len(carLot)):
     carLot[i].direction = assignDirection(random.random())
 
-# Car rank will
-# Do ordering of cars     #Stretch Goal: Implement the more robust way of the ordering
-#
-carOrdering = PriorityQueue(4)
-for i in range(len(carLot)):
-    index = i;
-    adjustIndex = -(carLot[i].carIndex)  # make index value so that we can prioritize lower indexes
-    carRank = (carLot[i].waitTime + adjustIndex)  # Calculate the rank of the given car; Wait time + lower index
-    # print("CAR#"+str(i)+" has rank of " + str(carRank))
-    # Multiply by -1 so that priority queue puts the "negative-most" values at top
-    carOrdering.put(-carRank, str(index))  # More wait time => more negative => More priority
 
+carOrdering = calculateNextOrder()
 
 # carLotState(carLot)
 
@@ -218,7 +229,8 @@ def processACycle(carOrdering):
 
         if checkDirection(next_car):  # If car is able to pass
             # store data
-            recordedPasses.insert((next_car.carIndex+1,next_car.waitTime,currentIntervalNumber))#Since we export data, I just add +1; car 0 becomes car 1, etc
+            #recordedPasses.insert([next_car.carIndex + 1, next_car.waitTime,currentIntervalNumber])  # Since we export data, I just add +1; car 0 becomes car 1, etc
+            recordedPasses
             carsPassedThroughIntersection = carsPassedThroughIntersection + 1  # add to global counter
             next_car.carPassed = True  # set g to true
             next_car.waitTime = 0  # reset wait time to 0 for next iteration
@@ -228,7 +240,6 @@ def processACycle(carOrdering):
             # If the car passed, all tiles necessary are zero, thus, we just change corresponding tiles to 1
             updateIntersection(next_car)
 
-
         else:
             next_car.waitTime = next_car.waitTime + 1
             next_car.carPassed = False
@@ -236,9 +247,9 @@ def processACycle(carOrdering):
         currentIntervalNumber = currentIntervalNumber + 1
 
 
-# for x in range(intervalLimit): #run the simulation interval limit = 10 times...
-processACycle(carOrdering)
-# Reorder cars again after processing each car
+for x in range(intervalLimit): #run the simulation interval limit = 10 times...
+    processACycle(carOrdering)
+    carOrdering = calculateNextOrder()
 
 print("FINSIHED")
 
